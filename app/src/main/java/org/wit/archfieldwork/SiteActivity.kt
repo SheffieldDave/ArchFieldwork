efb1799237eea6dc396482f531cf4cecff700fd7
SheffieldDave <simonstautner@live.com>
2019-01-11 22:28:49 +0100

    initial commit

---
+++ app/src/main/java/org/wit/archfieldwork/SiteActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.archfieldwork
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SiteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_site)
+    }
+}
