91d4d474caa203dfb849bc19b5d509110b5a3fe9
SheffieldDave <simonstautner@live.com>
2019-01-11 23:48:58 +0100

    card on two three

--- app/src/main/java/org/wit/archfieldwork/activities/SIteListActivity.kt
+++ app/src/main/java/org/wit/archfieldwork/activities/SIteListActivity.kt
@@ -2,8 +2,18 @@ package org.wit.archfieldwork.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_site_list.*
+import kotlinx.android.synthetic.main.card_site.view.*
+
 import org.wit.archfieldwork.R
 import org.wit.archfieldwork.main.MainApp
+import org.wit.archfieldwork.models.SiteModel
 
 class SiteListActivity : AppCompatActivity() {
 
@@ -13,5 +23,31 @@ class SiteListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_site_list)
         app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = SiteAdapter(app.sites)
+    }
+}
+
+class SiteAdapter constructor(private var sites: List<SiteModel>):RecyclerView.Adapter<SiteAdapter.MainHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        return MainHolder(LayoutInflater.from(parent?.context).inflate(R.layout.card_site,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val site = sites[holder.adapterPosition]
+        holder.bind(site)
+    }
+
+    override fun getItemCount(): Int  = sites.size
+
+    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        fun bind(site: SiteModel){
+            itemView.siteName.text = site.name
+            itemView.description.text = site.description
+        }
     }
 }
