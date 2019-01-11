e956a792ec3492a008c4f3c667bccf66a118055c
SheffieldDave <simonstautner@live.com>
2019-01-12 00:17:04 +0100

    -Site list -Site create -Name -Description -Add -cancel

--- app/src/main/java/org/wit/archfieldwork/activities/SiteActivity.kt
+++ app/src/main/java/org/wit/archfieldwork/activities/SiteActivity.kt
@@ -2,6 +2,8 @@ package org.wit.archfieldwork.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_site.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -21,6 +23,9 @@ class SiteActivity : AppCompatActivity(), AnkoLogger {
         setContentView(R.layout.activity_site)
         app = application as MainApp
 
+        toolbarAdd.title = title
+        setSupportActionBar(toolbarAdd)
+
         btnAdd.setOnClickListener() {
             site.name = siteName.text.toString()
             site.description = description.text.toString()
@@ -35,5 +40,20 @@ class SiteActivity : AppCompatActivity(), AnkoLogger {
                 toast ("Please Enter a title")
             }
         }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_site,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.item_cancel ->{
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
