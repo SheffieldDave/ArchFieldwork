32bf8f1da58ae9f70fe2a1224b4454c1acecc8d2
SheffieldDave <simonstautner@live.com>
2019-01-12 15:58:31 +0100

    shown google map of allsites

--- app/src/main/java/org/wit/archfieldwork/activities/SiteMapsActivity.kt
+++ app/src/main/java/org/wit/archfieldwork/activities/SiteMapsActivity.kt
@@ -1,23 +1,50 @@
 package org.wit.archfieldwork.activities
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import kotlinx.android.synthetic.main.activity_site_maps.*
 import org.wit.archfieldwork.R
 
-import kotlinx.android.synthetic.main.activity_site_maps.*
+import kotlinx.android.synthetic.main.content_site_maps.*
 
-class SiteMapsActivity : AppCompatActivity() {
+
+
+class SiteMapsActivity : AppCompatActivity(){
+
+    lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_site_maps)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setSupportActionBar(toolbarMaps)
+        mapView.onCreate(savedInstanceState)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 }
+
+
