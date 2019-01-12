6176e383da4dde7e9f7894681a1482b012f21808
SheffieldDave <simonstautner@live.com>
2019-01-12 16:31:57 +0100

    working google  show sites

--- app/src/main/java/org/wit/archfieldwork/activities/SiteMapsActivity.kt
+++ app/src/main/java/org/wit/archfieldwork/activities/SiteMapsActivity.kt
@@ -2,23 +2,46 @@ package org.wit.archfieldwork.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_site_maps.*
 import org.wit.archfieldwork.R
 
 import kotlinx.android.synthetic.main.content_site_maps.*
+import org.wit.archfieldwork.R.id.mapView
+import org.wit.archfieldwork.R.id.toolbarMaps
+import org.wit.archfieldwork.main.MainApp
 
 
-
-class SiteMapsActivity : AppCompatActivity(){
+class SiteMapsActivity : AppCompatActivity(), GoogleMap.OnMarkerClickListener{
 
     lateinit var map: GoogleMap
+    lateinit var app:MainApp
+
+    fun configureMap() {
+        map.uiSettings.setZoomControlsEnabled(true)
+        app.sites.findAll().forEach {
+            val loc = LatLng (it.lat, it.lng)
+            val options = MarkerOptions().title(it.name).position(loc)
+            map.addMarker(options).tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc,it.zoom))
+            map.setOnMarkerClickListener(this)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_site_maps)
         setSupportActionBar(toolbarMaps)
-        mapView.onCreate(savedInstanceState)
+        app = application as MainApp
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
     }
 
     override fun onDestroy() {
@@ -44,6 +67,11 @@ class SiteMapsActivity : AppCompatActivity(){
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
         mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        currentTitle.text = marker.title
+        return false
     }
 }
 
